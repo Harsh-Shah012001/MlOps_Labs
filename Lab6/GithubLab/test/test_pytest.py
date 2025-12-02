a232a0d6d2927ffcfb2d8a441dd29c7ffba0b946
Harsh-Shah012001 <shah.harsh8@northeastern.edu>
2025-12-02 00:08:58 -0500

    Updated test file

--- Lab6/GithubLab/test/test_pytest.py
+++ Lab6/GithubLab/test/test_pytest.py
@@ -1,5 +1,11 @@
-import pytest
+import sys
+import os
+
+# Add parent directory to path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from src import temperature
+
 
 def test_celsius_to_fahrenheit():
     assert temperature.celsius_to_fahrenheit(0) == 32
